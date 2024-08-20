03fc6078b10fa8214bef64776b352e9f3ae07f34
krokodil <kirilltoptun@gmail.com>
2024-08-20 21:52:58 +1200

    finished Skill section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -10,36 +10,36 @@ export const Skills = () => {
   return (
     <section id='skills' className={styles.container}>
         <h2 className={styles.title}>Skills</h2>
-        <div>
-            <ul>
-                {skills.map((skillItem, id) => {
-                    const isHovered = hoveredIndex === id;
-                    return (
-                        <li 
-                          key={id} 
-                          className={styles.skillItem}
-                          onMouseEnter={() => setHoveredIndex(id)}
-                          onMouseLeave={() => setHoveredIndex(null)}
-                        >
-                            <img src={getImageUrl(skillItem.imageSrc)} alt={`${skillItem.title} Image`}/>
-                            <div className={styles.skillItemInfo}>
-                                <h3>{skillItem.title}</h3>
-                                <div className={styles.progressBarBackground}>
-                                    <div 
-                                      className={styles.progressBar} 
-                                      style={{ 
-                                        width: `${skillItem.progress}%`, 
-                                        backgroundColor: `#${skillItem.colour}`,
-                                        boxShadow: isHovered ? `0 0 8px ${hexToRgb(skillItem.colour)}` : 'none'
-                                      }}
-                                    >
-                                    </div>
+        <div className={styles.gridContainer}>
+            {skills.map((skillItem, id) => {
+                const isHovered = hoveredIndex === id;
+                return (
+                    <div 
+                        key={id} 
+                        className={styles.skillItem}
+                        onMouseEnter={() => setHoveredIndex(id)}
+                        onMouseLeave={() => setHoveredIndex(null)}
+                    >
+                        <div className={styles.imgdiv}>
+                        <img src={getImageUrl(skillItem.imageSrc)} alt={`${skillItem.title} Image`}/>
+                        </div>
+                        <div className={styles.skillItemInfo}>
+                            <h3>{skillItem.title}</h3>
+                            <div className={styles.progressBarBackground}>
+                                <div 
+                                    className={styles.progressBar} 
+                                    style={{ 
+                                    width: `${skillItem.progress}%`, 
+                                    backgroundColor: `#${skillItem.colour}`,
+                                    boxShadow: isHovered ? `0 0 8px ${hexToRgb(skillItem.colour)}` : 'none'
+                                    }}
+                                >
                                 </div>
                             </div>
-                        </li>
-                    );
-                })}
-            </ul>
+                        </div>
+                    </div>
+                );
+            })}
         </div>
     </section>
   )
